36bd905da79ffb40a13dcdbbf7f3f226a3fa6488
Steve <steve.maina@student.moringaschool.com>
2022-09-06 23:07:44 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -4,5 +4,5 @@
 main.remove();
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
-newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.innerHTML = "YOUR NAME is the champion";
 document.body.append(newHeader);
