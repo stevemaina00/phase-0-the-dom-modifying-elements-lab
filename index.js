d4df0e3008ceae426fb5dccbda85f0f8cad5667c
Steve <steve.maina@student.moringaschool.com>
2022-09-06 23:06:07 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Write your code here!
-document.getElementById('main').remove();
 
-const newHeader = document.createElement('h1')
-newHeader.id = 'victory'
-newHeader.innerHTML = "YOUR-NAME is the champion"
+
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.append(newHeader);
